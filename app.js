22105a4474e92e743fd3aab6c9e5c619607bb59b
Steven Woodward <steveanthony999@gmail.com>
2020-08-13 22:30:58 -0700

    added dependencies

---
+++ app.js
@@ -0,0 +1,20 @@
+// Tech Used
+
+// express
+// bcrypt
+// jsonwebtoken
+// mongoose
+// passport
+// passport-jwt
+// cors
+// dotenv
+// express-validator
+
+// multer
+// datauri
+// cloudinary
+
+// jade
+// @sendgrid/mail
+
+// nodemon
